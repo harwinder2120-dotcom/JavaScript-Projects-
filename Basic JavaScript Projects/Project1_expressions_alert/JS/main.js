684d8bae483ba2086f7488b415efa90b71b26b68
harwinder2120-dotcom <harwinder2120@gmail.com>
2026-03-02 22:22:54 -0500

    Completed Project1_expressions_alert assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+// This displays a simple alert message
+window.alert("Welcome to my JavaScript project!");
+
+// This writes text directly to the webpage
+document.write("This text is written using document.write().<br><br>");
+
+// These two variables store parts of a sentence
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// This concatenates Sent1 and Sent2 and writes them to the page
+document.write(Sent1 + Sent2 + "<br><br>");
+
+// This is an example of a JavaScript expression
+// It adds two numbers together and displays the result
+document.write("5 + 7 = " + (5 + 7) + "<br><br>");
+
+// This is a string containing an apostrophe and escaped characters
+var escapedString = "It\'s important to escape characters like quotes: \"Hello!\"";
+
+// Display the escaped string in an alert
+window.alert(escapedString);
